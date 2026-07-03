9511c9cab6caadda5958ad365ff065dad5945a7f
RHermanet14 <rhermanet14@gmail.com>
2026-07-02 22:33:03 -0400

    stripped project of all introductory fluff

---
+++ app/send/page.tsx
@@ -0,0 +1,13 @@
+export default function Send() {
+  return (
+    <div>
+      <h1 className="">This is where you can send files</h1>
+      <div>
+        <p>Enter secure password or whatever to safely send stuff like files</p>
+        <input>
+        </input>
+      </div>
+      
+    </div>
+  );
+}
